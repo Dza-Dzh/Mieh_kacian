9907ed3be1e4569644fe3ebf05bb89992b44a822
Miellcode <jilanrokan@gmail.com>
2026-04-02 22:34:55 +0700

    menambahkan deklarasi struct waktu dan data kendaraan

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
+string jenisKendaraan[2] = {"mobil, motor"};
+
+struct waktu{
+    int jam;
+    int menit;
+};
+
+struct dataKendaraan{
+    string platNomor;
+    string jenisKendaraan;
+    int idParkir;
+    waktu masuk;
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+};
