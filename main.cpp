06020cfffdf0a7e28d3a5431ba61f4c3293cd211
Dza-Dzh <dzakydzhn@student.upi.edu>
2026-04-03 18:40:23 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,6 +23,31 @@ int generateID() {
     return IDselanjutnya++;
 }
 
+int cariIndex(dataKendaraan parkir[], int jumlah, string plat){
+    for(int i=0;i<jumlah;i++){
+        if(parkir[i].platNomor == plat)
+            return i;
+    }
+    return -1;
+}
+
+int hitungDurasiJam(waktu masuk, int jamKeluar, int menitKeluar){
+    int totalMasuk = masuk.jam * 60 + masuk.menit;
+    int totalKeluar = jamKeluar * 60 + menitKeluar;
+
+    int selisihMenit = totalKeluar - totalMasuk;
+    int durasiJam = selisihMenit / 60;
+
+    if(selisihMenit % 60 != 0)
+        durasiJam++;
+
+    if(durasiJam <= 0)
+        durasiJam = 1;
+
+    return durasiJam;
+}
+
+
 void tambahKendaraan(dataKendaraan parkir[], int &jumlah){
     cout << "Masukkan plat nomor kendaraan (Contoh: ABC 1234 XYZ): ";
     cin >> parkir[jumlah].platNomor;
@@ -71,6 +96,46 @@ void tambahKendaraan(dataKendaraan parkir[], int &jumlah){
     cout << "Data Kendaraan berhasil ditambahkan\n";
 }
 
+
+void kendaraanKeluar(dataKendaraan parkir[], int &jumlah){
+    cout << "\n=== CHECK OUT PARKIR ===\n";
+    string plat;
+    cout << "Masukkan plat nomor: ";
+    cin >> plat;
+
+    int index = cariIndex(parkir, jumlah, plat);
+
+    if(index == -1){
+        cout << "Kendaraan tidak ditemukan!\n";
+        return;
+    }
+
+    int jamKeluar, menitKeluar;
+    cout << "Jam keluar   : "; cin >> jamKeluar;
+    cout << "Menit keluar : "; cin >> menitKeluar;
+
+    int durasi = hitungDurasiJam(parkir[index].masuk, jamKeluar, menitKeluar);
+
+    int tarif = (parkir[index].jenisKendaraan == "motor") ? 2000 : 5000;
+    int totalBayar = durasi * tarif;
+
+    cout << "\n========== STRUK PARKIR ==========\n";
+    cout << "ID Parkir : " << parkir[index].idParkir << endl;
+    cout << "Plat      : " << parkir[index].platNomor << endl;
+    cout << "Jenis     : " << parkir[index].jenisKendaraan << endl;
+    cout << "Durasi    : " << durasi << " jam\n";
+    cout << "Bayar     : Rp " << totalBayar << endl;
+    cout << "==================================\n";
+
+    for(int i=index;i<jumlah-1;i++)
+        parkir[i] = parkir[i+1];
+
+    jumlah--;
+
+    cout << "Kendaraan berhasil keluar!\n";
+}
+
+
 int main() {
     dataKendaraan parkir[100];
     int jumlah = 0;
@@ -89,6 +154,9 @@ while (true)
 
     if(pilihan == 1){
         tambahKendaraan(parkir, jumlah);
+    }
+    else if(pilihan == 4){
+    kendaraanKeluar(parkir, jumlah);
     }
     else if (pilihan == 0){
         break;
