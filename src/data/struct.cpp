eca8c2466445da69f91684ac9e0faec7610965ba
NaufalSalva <mhmmdnaufalh@gmail.com>
2026-04-04 10:15:42 +0700

    Update structure folder agar terlihat rapih:

---
+++ src/data/struct.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+string jenisKendaraan[2] = {"motor", "mobil"};
+
+struct waktu{
+    int jam;
+    int menit;
+};
+
+struct dataKendaraan{
+    string platNomor;
+    string jenis;
+    int idParkir;
+    waktu masuk;
+    dataKendaraan* next;
+};
